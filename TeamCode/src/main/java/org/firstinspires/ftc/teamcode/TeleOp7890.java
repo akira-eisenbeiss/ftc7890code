0ba55711bf064eace5d2dfd148ae2596940a4a35
Ailuridaek3k <a0z.universe@gmail.com>
2017-10-08 10:31:09 -0400

    fixed typos and mistakes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp7890.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp7890.java
@@ -36,20 +36,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
 public class TeleOp7890 extends LinearOpMode {
 
@@ -60,7 +46,7 @@ public class TeleOp7890 extends LinearOpMode {
     public final static String RIGHTBACK = "rightBack";
     //names of the intake wheels
     public final static String LEFTINTAKE = "leftIntake";
-    public final static String RIGHTINTAKE = "leftIntake";
+    public final static String RIGHTINTAKE = "rightIntake";
     //name of the motor that does the lift
     //public final static String LIFTMOTOR = "liftMotor";
 
@@ -120,8 +106,8 @@ public class TeleOp7890 extends LinearOpMode {
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
-            double intakeLeft;
-            double intakeRight;
+            //double intakeLeft;
+            //double intakeRight;
             //double liftRaise;
             //double liftLower;
 
