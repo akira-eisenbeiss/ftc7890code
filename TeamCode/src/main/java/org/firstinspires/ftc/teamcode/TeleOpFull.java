9958db9971e796edc3d05389a5373fd3a68f1107
ErinZ1234567890 <erin_zhao@horacemann.org>
2017-11-05 14:14:00 -0500

    Update TeleOpFull.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFull.java
@@ -59,7 +59,7 @@ public class TeleOpFull extends OpMode {
         liftMotor = hardwareMap.get(DcMotor.class, LIFTMOTOR);
         //servos
         //leftClamp = hardwareMap.get(Servo.class, LEFTCLAMP);
-      //  rightClamp = hardwareMap.get(Servo.class, RIGHTCLAMP);
+        //  rightClamp = hardwareMap.get(Servo.class, RIGHTCLAMP);
 
 //hiiii
         //motor directions
@@ -79,91 +79,88 @@ public class TeleOpFull extends OpMode {
     @Override
     public void loop(){
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
-            double liftRaise;
-            double liftLower;
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
+        double liftRaise;
+        double liftLower;
 
-            // POV drivings controls
-            float drive = -gamepad1.left_stick_y;
-            float turn = gamepad1.right_stick_x;
-            leftPower = Range.clip(drive + turn, -1.0, 1.0);
-            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+        // POV drivings controls
+        float drive = -gamepad1.left_stick_y;
+        float turn = gamepad1.right_stick_x;
+        leftPower = Range.clip(drive + turn, -1.0, 1.0);
+        rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
         /*
             //tank mode version
             leftPower  = -gamepad1.left_stick_y ;
             rightPower = -gamepad1.right_stick_y ;
         */
-            //wheel lift mechanism controls
-            float leftTrigger1 = gamepad1.left_trigger;
-            float rightTrigger1 = gamepad1.right_trigger;
-            boolean gamepad1A = gamepad1.a;
-            boolean gamepad1B = gamepad1.b;
-            liftRaise = Range.clip(leftTrigger1, -0.75, 0.75);
-            liftLower = Range.clip(rightTrigger1, 0.75, -0.75);
+        //wheel lift mechanism controls
+        float leftTrigger1 = gamepad1.left_trigger;
+        float rightTrigger1 = gamepad1.right_trigger;
+        boolean gamepad1A = gamepad1.a;
+        boolean gamepad1B = gamepad1.b;
+        liftRaise = Range.clip(leftTrigger1, -0.75, 0.75);
+        liftLower = Range.clip(rightTrigger1, 0.75, -0.75);
 
-            // Send calculated power to wheels
-            leftFront.setPower(leftPower);
-            leftBack.setPower(leftPower);
-            rightFront.setPower(rightPower);
-            rightBack.setPower(rightPower);
+        // Send calculated power to wheels
+        leftFront.setPower(leftPower);
+        leftBack.setPower(leftPower);
+        rightFront.setPower(rightPower);
+        rightBack.setPower(rightPower);
 
-            //the ifs that control the lift mechanism
-            if (leftTrigger1 > 0) {
-                liftMotor.setPower(liftRaise);
-            } else {
-                liftMotor.setPower(leftTrigger1);
-            }
-            if (rightTrigger1 > 0) {
-                liftMotor.setPower(liftLower);
-            } else {
-                liftMotor.setPower(rightTrigger1);
-            }
+        //the ifs that control the lift mechanism
+        if (leftTrigger1 > 0) {
+            liftMotor.setPower(liftRaise);
+        } else {
+            liftMotor.setPower(leftTrigger1);
+        }
+        if (rightTrigger1 > 0) {
+            liftMotor.setPower(liftLower);
+        } else {
+            liftMotor.setPower(rightTrigger1);
+        }
 
-            int x = 0;
-            //the ifs that are used for toggling the intake mechanism
-            if (gamepad1A) {
-                x++;
-                if (x % 2 != 0) {
-                    leftIntake.setPower(1.0);
-                    rightIntake.setPower(1.0);
-                } else if (x % 2 == 0){
-                    leftIntake.setPower(0.0);
-                    rightIntake.setPower(0.0);
-                }
+        int x = 0;
+        //the ifs that are used for toggling the intake mechanism
+        if (gamepad1A) {
+            x++;
+            if (x % 2 != 0) {
+                leftIntake.setPower(1.0);
+                rightIntake.setPower(1.0);
+            } else if (x % 2 == 0){
+                leftIntake.setPower(0.0);
+                rightIntake.setPower(0.0);
             }
-            int y = 0;
-            if (gamepad1B) {
-                y++;
-                if (y % 2 != 0) {
-                    leftIntake.setPower(-1.0);
-                    rightIntake.setPower(-1.0);
-                } else if (y % 2 == 0){
-                    leftIntake.setPower(0.0);
-                    rightIntake.setPower(0.0);
-                }
+        }
+        int y = 0;
+        if (gamepad1B) {
+            y++;
+            if (y % 2 != 0) {
+                leftIntake.setPower(-1.0);
+                rightIntake.setPower(-1.0);
+            } else if (y % 2 == 0){
+                leftIntake.setPower(0.0);
+                rightIntake.setPower(0.0);
             }
-
-/*
+        }
             //servos
             boolean gamepad2A = gamepad2.a;
             boolean gamepad2B = gamepad2.b;
-            int a = 0;
             if (gamepad2A) {
-                leftClamp.setPosition(1.0)
-                rightClamp.setPosition(-1.0)
+                leftClamp.setPosition(0)
+                rightClamp.setPosition(0)
             }
             if (gamepad2B){
                 leftClamp.setPosition(-1.0);
                 rightClamp.setPosition(1.0);
             }
-*/
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.update();
     }
-}
