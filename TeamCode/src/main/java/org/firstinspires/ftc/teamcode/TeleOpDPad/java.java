d6e8c0fe3714f233b9188407a6fab88c8ef6b818
Ailuridaek3k <a0z.universe@gmail.com>
2017-11-10 18:37:40 -0500

    regular features working w/ mechanum
    but unfortunado that the strafing isnt quite there yet :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDPad/java.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDPad/java.java
@@ -8,8 +8,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="complete tele op", group="Tele Op")
-public class TeleOpFull extends OpMode {
+@TeleOp(name="test", group="Tele Op")
+public class java extends OpMode {
 
     public final static String LEFTFRONT = "leftFront";
     public final static String LEFTBACK = "leftBack";
@@ -85,58 +85,22 @@ public class TeleOpFull extends OpMode {
         double rightPower;
         double liftRaise;
         double liftLower;
-
-        double frontStrafe;
         double backStrafe;
+        double frontStrafe;
 
-
-
-        // POV drivings controls
-        float drive = -gamepad1.left_stick_y;
-        float turn = gamepad1.right_stick_x;
-        leftPower = Range.clip(drive + turn, -1.0, 1.0);
-        rightPower = Range.clip(drive - turn, -1.0, 1.0);
-
-
-        //strafing
-        float strafe = gamepad1.left_stick_x;
-
-        frontStrafe = Range.clip(-strafe, -1.0, 1.0);
-        backStrafe = Range.clip(strafe, -1.0, 1.0);
-
-
-        //an if statement for the strafing
-        if (strafe > 0) {
-            leftFront.setPower(frontStrafe);
-            rightFront.setPower(frontStrafe);
-            leftBack.setPower(backStrafe);
-            rightBack.setPower(backStrafe);
+        if (gamepad1.dpad_up) {
+            leftFront.setPower(1);
+            leftBack.setPower(1);
+            rightFront.setPower(1);
+            rightBack.setPower(1);
         }
-        else if (strafe < 0) {
-            leftFront.setPower(backStrafe);
-            rightFront.setPower(backStrafe);
-            leftBack.setPower(frontStrafe);
-            rightBack.setPower(frontStrafe);
+        else if (gamepad1.dpad_down){
+            leftFront.setPower(-1);
+            leftBack.setPower(-1);
+            rightFront.setPower(-1);
+            rightBack.setPower(-1);
         }
-
-
-        //wheel lift mechanism controls
-        float leftTrigger1 = gamepad1.left_trigger;
-        float rightTrigger1 = gamepad1.right_trigger;
-        boolean gamepad1A = gamepad1.a;
-        boolean gamepad1B = gamepad1.b;
-        liftRaise = Range.clip(leftTrigger1, -0.75, 0.75);
-        liftLower = Range.clip(rightTrigger1, 0.75, -0.75);
-
-        // Send calculated power to wheels
-        leftFront.setPower(leftPower);
-        leftBack.setPower(leftPower);
-        rightFront.setPower(rightPower);
-        rightBack.setPower(rightPower);
-
-        /*
-        //strafe with dpad
-        if (gamepad1.dpad_right){
+        else if (gamepad1.dpad_right){
             leftFront.setPower(1);
             leftBack.setPower(-1);
             rightFront.setPower(-1);
@@ -154,70 +118,6 @@ public class TeleOpFull extends OpMode {
             rightFront.setPower(0);
             rightBack.setPower(0);
         }
-        */
-
-        /*
-        //strafing power
-        leftFront.setPower(frontStrafe);
-        rightFront.setPower(frontStrafe);
-        leftBack.setPower(backStrafe);
-        rightBack.setPower(backStrafe);
-        */
-
-        //the ifs that control the lift mechanism
-        if (leftTrigger1 > 0) {
-            liftMotor.setPower(liftRaise);
-        } else {
-            liftMotor.setPower(leftTrigger1);
-        }
-        if (rightTrigger1 > 0) {
-            liftMotor.setPower(liftLower);
-        } else {
-            liftMotor.setPower(rightTrigger1);
-        }
-
-        int x = 0;
-        //the ifs that are used for toggling the intake mechanism
-        if (gamepad1A) {
-            x++;
-            if (x % 2 != 0) {
-                leftIntake.setPower(1.0);
-                rightIntake.setPower(1.0);
-            } else if (x % 2 == 0) {
-                leftIntake.setPower(0.0);
-                rightIntake.setPower(0.0);
-            }
-        }
-        int y = 0;
-        if (gamepad1B) {
-            y++;
-            if (y % 2 != 0) {
-                leftIntake.setPower(-1.0);
-                rightIntake.setPower(-1.0);
-            } else if (y % 2 == 0) {
-                leftIntake.setPower(0.0);
-                rightIntake.setPower(0.0);
-            }
-        }
-
-        /*
-            //servos
-            boolean gamepad2A = gamepad2.a;
-            boolean gamepad2B = gamepad2.b;
-            if (gamepad2A) {
-                leftClamp.setPosition(0)
-                rightClamp.setPosition(0)
-            }
-            if (gamepad2B){
-                leftClamp.setPosition(-1.0);
-                rightClamp.setPosition(1.0);
-            }
-*/
-
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-        telemetry.update();
     }
 }
+
