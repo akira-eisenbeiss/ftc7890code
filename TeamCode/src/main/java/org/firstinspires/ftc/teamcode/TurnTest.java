e13a9724dcea936b759230c92443e636a3868705
ErinZ1234567890 <erin_zhao@horacemann.org>
2018-01-12 20:44:29 -0500

    Create TurnTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurnTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="TurnTest", group="Linear Op")
+public class TurnTest extends LinearOpMode {
+    
+    DcMotor leftFront, leftBack, rightFront, rightBack;
+    
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftBack = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("leftFront");
+        rightBack = hardwareMap.dcMotor.get("leftFront");
+        
+        waitForStart();
+        //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+        
+        while (opModeIsActive()) {
+            leftFront.setPower(0.5);
+            leftBack.setPower(0.5);
+            rightFront.setPower(0.5);
+            rightBack.setPower(0.5);
+
+        }
+    }
+}
